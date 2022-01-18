563f7f8c1cbd75d2ee0c6d41f27af6d9a9dbd0dd
owis <bcabezasr@gmail.com>
2022-01-18 10:28:05 -0300

    deploy + ecosystem config

--- deploy/ecosystem.config.js
+++ deploy/ecosystem.config.js
@@ -1,12 +1,14 @@
 module.exports = {
     apps:[
-        name: "randomAPI",
-        script: "npm run start",
+        {
+        name: 'randomAPI',
+        script: 'npm run start',
         instances: 3,
         watch: true,
         increment_var: 'PORT',
         env: {
             'PORT': 5000,
         },
+    },
     ],
 };
